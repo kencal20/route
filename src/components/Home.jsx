15a4124d0a5736672c94f7a750b866cbe89c4157
kencal20 <kenadjei20@gmail.com>
2022-01-26 23:55:37 +0000

    home file

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse neque
+          ratione deleniti et ullam porro veniam, at, mollitia, assumenda eaque
+          blanditiis obcaecati voluptas! Laborum odit possimus, recusandae
+          libero inventore rem!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
