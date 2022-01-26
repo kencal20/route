fe51697a56967ecb63f322ae44c5cd4ea1115833
kencal20 <kenadjei20@gmail.com>
2022-01-26 23:55:26 +0000

    contact file

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio rerum et
+        repellat, corrupti eaque nihil, debitis distinctio voluptas atque
+        temporibus non magnam, sed similique voluptatibus tenetur eligendi
+        dolores. Enim, ad.
+      </p>
+    </div>
+  );
+};
+
+export default Contacts;
