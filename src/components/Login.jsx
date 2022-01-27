90e546596f147b26f564718664032e85b317cc93
kencal20 <kenadjei20@gmail.com>
2022-01-27 08:53:34 +0000

    button additions and imports

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,18 @@
-
-
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
-import '../Main_styles.css'
-
-
+import "../Main_styles.css";
 
 const Login = () => {
-    return (
-        
-            
-      <>
+  return (
+    <>
       <div>
         <div className="App">
-            <h1>Login</h1>
-          <Form >
-
+          <h1>Login</h1>
+          <Form>
             <div className="Username">
               <label>Username</label>
-              <input
-                type="text"
-                placeholder="username"
-                required
-             
-              />
+              <input type="text" placeholder="username" required />
             </div>
             <div className="Password">
               <label>Password</label>
@@ -31,15 +21,31 @@ const Login = () => {
                 placeholder="password"
                 name="Password"
                 required
-               
               />
             </div>
+            <Button
+              type="submit"
+              style={{
+                background: "dodgerblue",
+                width: "10%",
+                height: "30px",
+                margin: "20px",
+                border: ["1px", "dodgerblue", "solid"],
+                borderRadius: "6px",
+                color: "white",
+              }}
+            >
+              login
+            </Button>
+            <p>New Here?</p>
+            <Link to="/signup">
+              <p>Signup</p>
+            </Link>
           </Form>
         </div>
-        </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Login;
-
